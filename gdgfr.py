53e6859da4d75f3a7f1e22811249621222c58117
Nf-Jza <josa241102@gmail.com>
2024-02-20 21:47:41 +0700

    20Feb-24

--- gdgfr.py
+++ gdgfr.py
@@ -1,6 +1,6 @@
 #format scriptDate = hhmmddMMyyyy
 scriptDate = '03Feb-1241'
-registered = ['josa241102','mbelbanany']
+registered = ['josa241102','mbelbanany','khusnulkhotimah120797']
 latestScript = 'https://raw.githubusercontent.com/Nf-Jza/RKSCinstaller/main/bot.py'
 
 #remove userInfo.py
