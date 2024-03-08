215425afdcb8ece4fe349dbf162b7a53a1dd300f
Nf-Jza <josa241102@gmail.com>
2024-03-08 08:58:15 +0700

    08Mar-0821

--- scInstaller.py
+++ scInstaller.py
@@ -3,24 +3,24 @@ import subprocess
 import os
 
 pastebin_url = 'https://raw.githubusercontent.com/Nf-Jza/RKSCinstaller/main/bot.py'
-response = requests.get(pastebin_url)
+# response = requests.get(pastebin_url)
 current_directory = os.path.dirname(os.path.abspath(__file__))
 script_namespace = {}
 
 
-def getScript():
-    global script_namespace
-    if response.status_code == 200:
-        script_text = response.text
-    else:
-        print("Failed to retrieve the script. Maybe check your internet connection.")
-    try:
-        exec(script_text, script_namespace)
-    except Exception as e:
-        print(f"Error executing the script: {e}")
-
-
-getScript()
+# def getScript():
+#     global script_namespace
+#     if response.status_code == 200:
+#         script_text = response.text
+#     else:
+#         print("Failed to retrieve the script. Maybe check your internet connection.")
+#     try:
+#         exec(script_text, script_namespace)
+#     except Exception as e:
+#         print(f"Error executing the script: {e}")
+#
+#
+# getScript()
 
 
 
