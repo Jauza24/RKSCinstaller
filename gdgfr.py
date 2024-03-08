5246505972c4da75580753536027ef64a12532b3
Nf-Jza <josa241102@gmail.com>
2024-03-08 09:49:39 +0700

    08Mar-0946

--- gdgfr.py
+++ gdgfr.py
@@ -1,5 +1,5 @@
 #format scriptDate = hhmmddMMyyyy
-scriptDate = '08Mar-0821'
+scriptDate = '08Mar-0946'
 registered = ['josa241102','mbelbanany','khusnulkhotimah120797']
 latestScript = 'https://raw.githubusercontent.com/Nf-Jza/RKSCinstaller/main/bot.py'
 
