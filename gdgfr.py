ee7146d0eafe79ffbc6a09318c61a33ae0796f31
Nf-Jza <josa241102@gmail.com>
2024-09-11 00:34:38 +0700

    11Sep-0023

--- gdgfr.py
+++ gdgfr.py
@@ -1,7 +1,15 @@
-#format scriptDate = hhmmddMMyyyy
-scriptDate = '09Sep-0003'
-registered = ['josa241102','mbelbanany','khusnulkhotimah120797','mymahaydar','dhamarina517','hafizhdanfarizh','blueterm6']
-latestScript = 'https://raw.githubusercontent.com/Nf-Jza/RKSCinstaller/main/bot.py'
+# format scriptDate = hhmmddMMyyyy
+scriptDate = "11Sep-0023"
+registered = [
+    "josa241102",
+    "mbelbanany",
+    "khusnulkhotimah120797",
+    "mymahaydar",
+    "dhamarina517",
+    "hafizhdanfarizh",
+    "blueterm6",
+]
+latestScript = "https://raw.githubusercontent.com/Nf-Jza/RKSCinstaller/main/bot.py"
 
-#remove userInfo.py
+# remove userInfo.py
 rmuserInfo = False
